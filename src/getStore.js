c261e2cf222cceea8e71535ed02ae09c6f241b96
kwiecien <k.d.kwiecinski@gmail.com>
2019-03-06 22:50:16 +0100

    3: creating normalized default state

--- src/getStore.js
+++ src/getStore.js
@@ -1,9 +1,17 @@
 import {
     createStore
 } from 'redux';
+import {users} from "../server/db";
+import {getDefaultState} from "../server/getDefaultState";
+import {initializeDB} from "../server/db/initializeDB";
+
+initializeDB();
+const currentUser = users[0];
+const defaultState = getDefaultState(currentUser);
+console.log(defaultState);
 
 const reducer = state => state;
 
-const store = createStore(reducer);
+const store = createStore(reducer, defaultState);
 
-export const getStore = ()=>store;
+export const getStore = () => store;

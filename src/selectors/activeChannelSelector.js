4317e6c1f86e71b27c21dd51f0316e2cb6856271
kwiecien <k.d.kwiecinski@gmail.com>
2019-03-12 20:48:49 +0100

    7: implement selectors

---
+++ src/selectors/activeChannelSelector.js
@@ -0,0 +1,9 @@
+import {createSelector} from 'reselect';
+
+export const activeChannelSelector = createSelector(
+    state => state.get(`activeChannel`),
+    state => state.get(`channels`),
+    (activeChannel, channels) => {
+        return channels.find(channel => channel.get(`id`) === activeChannel)
+    }
+);

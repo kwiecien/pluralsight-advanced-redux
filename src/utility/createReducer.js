2d5ea3f77a71f3f8d9e1691a4bfe37b328cd2b6a
kwiecien <k.d.kwiecinski@gmail.com>
2019-03-12 21:56:51 +0100

    7: add reducers

--- src/utility/createReducer.js
+++ src/utility/createReducer.js
@@ -1,4 +1,4 @@
-export const createReducer = (initialState, handlers)=>{
+export const createReducer = (initialState, handlers) => {
     return function reducer(state = initialState, action) {
         if (handlers.hasOwnProperty(action.type)) {
             return handlers[action.type](state, action)
